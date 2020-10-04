5ca6c0c2266757717721590f5e4eef526c4d3ae3
Paul Siekmeier <siekmeierpaul@gmail.com>
2020-10-03 22:07:07 -0500

    second office hours guided session

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,69 @@
+
+// Code was started during office hours guided by Dom 
 console.log('this is app.js');
 
 function DrawBubblechart(sampleId)
 {
     console.log(`DrawBubblechart (${sampleId})`);
+
+
 }
 
 function DrawBargraph(sampleId)
 {
-    console.log(`draw bar graph (${sampleId})`);
+    console.log(`DrawBargraph (${sampleId})`);
+
+    d3.json('samples.json').then(data => {
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        var yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks,
+            type: 'bar',
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: 'h'
+        }
+
+        var barLayout = {
+            title: 'Top 10 Bacteria Cultures Found',
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot('bar', [barData], barLayout);
+    });
 }
 
 function ShowMetadata(sampleId)
 {
     console.log(`ShowMetadata(${sampleId})`)
+
+    d3.json('samples.json').then((data) => {
+
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(md => md.id == sampleId);
+        var result = resultArray[0];
+
+        var panel = d3.select('#sample-metadata');
+        panel.html('');
+        
+        Object.entries(result).forEach(([key,value]) => {
+
+            var textToShow = `SampleId = ${sampleId}`;
+            panel.append('h6').text(textToShow);
+
+
+        })
+    
+    })
 }
 
 function optionChanged(sampleId)
@@ -47,6 +98,7 @@ function InitDashboard()
 
         DrawBargraph(sampleId);
         DrawBubblechart(sampleId);
+        ShowMetadata(sampleId);
     });
 }
 
